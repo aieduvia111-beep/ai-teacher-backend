96c9c08fd4f72c929cada36bf1f99ae8775fe95b
aieduvia111-beep <aieduvia111@gmail.com>
2026-04-26 22:51:24 +0200

    feat: capgo social login Google

--- android/app/src/main/java/com/eduvia/app/MainActivity.java
+++ android/app/src/main/java/com/eduvia/app/MainActivity.java
@@ -1,12 +1,12 @@
 package com.eduvia.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(android.os.Bundle savedInstanceState) {
-    registerPlugin(GoogleAuth.class);
+    registerPlugin(SocialLoginPlugin.class);
     super.onCreate(savedInstanceState);
   }
 }
