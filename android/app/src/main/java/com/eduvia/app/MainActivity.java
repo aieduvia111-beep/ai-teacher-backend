3d819c936370841fe8f4fa51fd80cdac130310f2
aieduvia111-beep <aieduvia111@gmail.com>
2026-05-06 17:22:34 +0200

    fix: voice natywny mikrofon

--- android/app/src/main/java/com/eduvia/app/MainActivity.java
+++ android/app/src/main/java/com/eduvia/app/MainActivity.java
@@ -2,11 +2,30 @@ package com.eduvia.app;
 
 import com.getcapacitor.BridgeActivity;
 import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+import android.webkit.WebView;
+import android.webkit.PermissionRequest;
+import com.getcapacitor.Bridge;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(android.os.Bundle savedInstanceState) {
     registerPlugin(GoogleAuth.class);
     super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    Bridge bridge = getBridge();
+    if (bridge != null) {
+      WebView webView = bridge.getWebView();
+      webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+      webView.setWebChromeClient(new android.webkit.WebChromeClient() {
+        @Override
+        public void onPermissionRequest(PermissionRequest request) {
+          request.grant(request.getResources());
+        }
+      });
+    }
   }
 }
