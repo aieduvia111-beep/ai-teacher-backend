57f836abc93472646ea3a64962f7646fb7daa475
aieduvia111-beep <aieduvia111@gmail.com>
2026-05-27 20:08:04 +0200

    Revert "fix: podpunkty jedno zadanie"
    This reverts commit 26f24ed487c8105b3868273ad824baf0d01f7077.

--- app/openai_vision.py
+++ app/openai_vision.py
@@ -114,7 +114,7 @@ def build_prompt(subject: str, mode: str, generate_similar: bool, show_explanati
         "- NIE POMIJAJ zadania ostatniego na stronie!\n"
         "- Jesli widzisz 6 zadan na zdjeciu - w JSON musi byc 6 obiektow\n"
         "- Jesli widzisz 5 zadan - 5 obiektow. Itd.\n"
-        "- KRYTYCZNE: Zadanie z podpunktami a b c to JEDEN obiekt w problems. NIE rozdzielaj podpunktow na osobne zadania!\n"
+        "- KRYTYCZNE: Podpunkty a) b) c) d) to CZESC jednego zadania - NIE osobne zadania! Jeden obiekt w problems[]\n"
         "- similar_problems: konkretne zadania z liczbami, nie nazwy tematow"
     )
 
